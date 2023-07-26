aa43d49e06e6022aa41261ffbeb31695989ad745
hongjumeow <hongjuj0329@kaist.ac.kr>
2023-07-26 13:03:11 +0900

    structurize with classes

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,20 +1,12 @@
 import { EventEmitter } from "events";
-import { 
-	AbstractActionManager,
-	ActionManager,
+import {
 	ArcRotateCamera,
-	Color3,
 	Engine,
-	ExecuteCodeAction,
 	HemisphericLight,
-	Mesh,
-	MeshBuilder,
 	Scene,
-	StandardMaterial,
 	Vector3,
 } from "@babylonjs/core";
 
-import { defined } from "../utils/type";
 import Marble from "./marble";
 
 export default class World {
@@ -22,12 +14,11 @@ export default class World {
 	public on = this._events.on.bind(this._events);
 	public once = this._events.once.bind(this._events);
 	public off = this._events.off.bind(this._events);
+	public emit = this._events.emit.bind(this._events);
 
 	private _canvas: HTMLCanvasElement;
 	private _engine: Engine;
 	private _camera: ArcRotateCamera;
-
-	private _bigMarble: Marble;
 
 	public scene: Scene;
 
@@ -37,26 +28,28 @@ export default class World {
 		this.scene = new Scene(this._engine);
 	}
 
-	private _onSceneReady = () => {
-		this._camera = new ArcRotateCamera("camera1", 0, 0, 10, new Vector3(4, 0, -10), this.scene);
+	private _onSceneReady = (): void => {
+		this._events.on("onDrop", function() {console.log("hi")});
+
+		this._camera = new ArcRotateCamera("camera1", 0, 0, 20, new Vector3(4, 0, -10), this.scene);
 		this._camera.setTarget(Vector3.Zero());
 		this._camera.attachControl(this._canvas, true);
 
-		const light = new HemisphericLight("light", new Vector3(0, 1, 0), this.scene);
+		const light = new HemisphericLight("light", new Vector3(0, 10, 0), this.scene);
 		light.intensity = 0.7;
-	
-		this._bigMarble = new Marble(
-			this.scene, {
-				name: "big_marble",
-				diameter: 10,
-				color: new Color3(1, 0.2, 0),
-				action: new ExecuteCodeAction(
-					ActionManager.OnPickUpTrigger, (e) => {
-						const mesh = e.meshUnderPointer;
-						alert(mesh?.name);
-					}
-				)
-			});
+
+		for (let i=0; i<20; i++) {
+			const marble = new Marble(this.scene, {
+				name: "marble_" + i,
+				diameter: 1,
+				textureImg:'earth.jpeg',
+				action:(e) => {alert(e.meshUnderPointer)}}
+			);
+		}
+	}
+
+	public dropMarbles = (): void => {
+		console.log("drop marbles");
 	}
 
 	public start = (): void => {
@@ -74,7 +67,7 @@ export default class World {
 		});
 	}
 
-	public resize = () => {
+	public resize = (): void => {
 		this._engine.resize();
 	}
 }
