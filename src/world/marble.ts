6c60700c513eb862c0b62da3405c3a5a212db7fc
hongjumeow <hongjuj0329@kaist.ac.kr>
2023-07-26 13:03:31 +0900

    working..

--- src/world/marble.ts
+++ src/world/marble.ts
@@ -1,12 +1,14 @@
 import {
-	AbstractActionManager,
 	ActionManager,
-	Color3, 
 	ExecuteCodeAction,
+	Color3,
 	Mesh,
 	MeshBuilder,
 	Scene,
 	StandardMaterial,
+	Texture,
+	ActionEvent,
+	Scalar,
 } from "@babylonjs/core";
 
 import { defined } from "../utils/type";
@@ -14,8 +16,8 @@ import { defined } from "../utils/type";
 type MeshFeatures = {
 	name: string,
 	diameter: number,
-	color: Color3,
-	action: ExecuteCodeAction,
+	textureImg?: string,
+	action: (e: ActionEvent) => void,
 }
 
 export default class Marble {
@@ -28,24 +30,43 @@ export default class Marble {
 	}
 
 	private _initMesh(meshFeatures: MeshFeatures) {
-		const { name, diameter, color, action } = meshFeatures;
+		const { name, diameter, textureImg, action } = meshFeatures;
 		this._mesh = MeshBuilder.CreateSphere(name, { diameter: diameter }, this._scene);
 
-		let material = new StandardMaterial(name + "_mat", this._scene);
-		material.alpha = 1;
-		material.diffuseColor = color;
-		this._mesh.material = material;
-
-		this._mesh.actionManager = new ActionManager(this._scene);
-		this._mesh.actionManager.registerAction(action);
+		this._setMaterial(textureImg);
+		this._setActionManager(action);
+		this._setRandomPosition();
 	}
 
-	private _ensureActionManager(mesh: Mesh): AbstractActionManager {
-		const actionManager = mesh.actionManager ?? new ActionManager(this._scene);
-		if (!defined(mesh.actionManager)) {
-			mesh.actionManager = actionManager;
+	private _setMaterial(textureImg?: string) {
+		let material = new StandardMaterial("mat");
+		if (defined(textureImg)) {
+			material.diffuseTexture = new Texture(textureImg);
+		} else {
+			material.diffuseColor = new Color3(1, 0.2, 0);
 		}
+		this._mesh.material = material;
+	}
 
-		return actionManager;
+	private _setActionManager(action: (e: ActionEvent) => void) {
+		const actionManager = this._mesh.actionManager ?? new ActionManager(this._scene);
+		if (!defined(this._mesh.actionManager)) {
+			this._mesh.actionManager = actionManager;
+		}
+		this._mesh.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickTrigger, action));
+	}
+
+	private _setRandomPosition() {
+		this._mesh.position.x = Scalar.RandomRange(-10, 10);
+		this._mesh.position.y = Scalar.RandomRange(-10, 10);
+		this._mesh.position.z = Scalar.RandomRange(-10, 10);
+	}
+
+	public getMesh() {
+		return this._mesh;
+	}
+	
+	public disposeMesh() {
+		return this._mesh.dispose();
 	}
 }
