1d09616bd98cf0151d9efc0128b600663f7c50c6
hongjumeow <hongjuj0329@kaist.ac.kr>
2023-07-30 09:36:57 +0900

    set up for gravity system

---
+++ src/world/gravitySystem.ts
@@ -0,0 +1,15 @@
+import {
+  Scene,
+} from "@babylonjs/core";
+
+class GravitySystem {
+
+  private _scene: Scene;
+
+  constructor(scene: Scene) {
+    this._scene = scene;
+  }
+
+};
+
+export default GravitySystem;
